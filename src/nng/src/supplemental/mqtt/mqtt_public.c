6208b9d431af9029649019d4deace99c26b7bcaa
wayne <yukang.wei@emqx.io>
2023-12-07 14:58:19 +0800

    * MDF [mqtt_public] bridge to be compatible with topic alias
    Signed-off-by: wayne <yukang.wei@emqx.io>

--- src/nng/src/supplemental/mqtt/mqtt_public.c
+++ src/nng/src/supplemental/mqtt/mqtt_public.c
@@ -301,6 +301,11 @@ nng_mqtt_msg_get_publish_dup(nng_msg *msg)
 int
 nng_mqtt_msg_set_publish_topic(nng_msg *msg, const char *topic)
 {
+    // topic alias
+    if (topic == NULL) {
+        return 0;
+    }
+    // wild card
 	if (strchr(topic, '#') != NULL || strchr(topic, '+') != NULL) {
 		return -1;
 	}
